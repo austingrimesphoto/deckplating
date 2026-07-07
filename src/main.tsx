9cf704170bd9bc5c883b01637b0dc65d62a25ac7
Chaplain Tracker <05_banking.wetland@icloud.com>
2026-07-07 11:19:43 -0400

    Safely initialize fresh PWA location acquisition

--- src/main.tsx
+++ src/main.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
-import './fresh-location';
+import './fresh-location-bridge';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
