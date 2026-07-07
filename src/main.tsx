e2c66c3c6a41f5776b118514bdf1c60a93dc2cd7
Chaplain Tracker <05_banking.wetland@icloud.com>
2026-07-07 11:18:05 -0400

    Use fresh PWA location acquisition before check-in screen mounts

--- src/main.tsx
+++ src/main.tsx
@@ -2,6 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import 'maplibre-gl/dist/maplibre-gl.css';
 import './styles.css';
+import './fresh-location';
 import App from './App';
 
 ReactDOM.createRoot(document.getElementById('root')!).render(
